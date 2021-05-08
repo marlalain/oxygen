ba5ce627490b7cc319334737e640aae95b4eb619
Paulo <elie9alb@gmail.com>
2021-05-08 15:41:04 -0300

    Upgraded touch; Fixed cat name

--- src/bin/touch.rs
+++ src/bin/touch.rs
@@ -1,31 +1,27 @@
-use std::{env, fs};
+use std::fs::File;
+use std::env;
 use seahorse::{App, Context, Flag, FlagType};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let app = App::new("cat")
-        .description("Prints the content of files")
+    let app = App::new("touch")
+        .description("Creates an empty file")
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
-        .usage("cat file1 [files2+]")
-        .action(cat)
+        .usage("touch file1 [files2+]")
+        .action(touch)
         .flag(Flag::new("verbose", FlagType::Bool)
               .description("Becomes more verbose")
               .alias("v"),
-        )
-        ;
+        );
     app.run(args);
 }
 
-fn cat(c: &Context) {
+fn touch(c: &Context) {
     for arg in &c.args {
-        let content = fs::read_to_string(arg);
+        let mut _file = File::create(arg);
         if c.bool_flag("verbose") {
-            println!("Opening file {:?}", &arg);
-        }
-        match content {
-            Ok(ok) => { print!("{}", ok); }
-            Err(..) => { panic!(); }
+            println!("Created file '{}'.", arg);
         }
     }
 }
