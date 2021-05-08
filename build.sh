5719b1bdde4352ac3a11a0402594d5c39bff5fc1
Paulo <elie9alb@gmail.com>
2021-05-08 11:34:21 -0300

    Build script eye candy

--- build.sh
+++ build.sh
@@ -5,6 +5,15 @@ DESTDIR="build"
 SRCDIR="src"
 MAKESH="make.sh"
 
-mkdir -pv $DESTDIR
+red="\e[0;91m"
+green="\e[0;92m"
+blue="\e[0;94m"
+reset="\e[0m"
+
+echo -e "${red}=>${reset} Cleaning old build folder..."
+rm -rf $DESTDIR
+echo -e "${green}=>${reset} Creating '$DESTDIR'"
+mkdir -p $DESTDIR
 find $SRCDIR -type d ! -name $SRCDIR -execdir \
 	sh -c "cd {}; sh make.sh" \;
+echo -e "${blue}=>${reset} Done"
--- src/touch/make.sh
+++ src/touch/make.sh
@@ -6,11 +6,13 @@ NAME=${PWD##*/} # sh magic;; TODO test on other shells
 IN=$NAME".rs"
 OUT=$NAME
 DESTDIR="../../build/" # default value
+green="\e[0;92m"       # colors
+reset="\e[0m"          # colors
 
 # Following flags can be removed if the target system has the necessary libraries.
 # TODO add options
 FLAGS="-C target-feature=+crt-static"
 
 #$(C) $(IN) -o $(DESTDIR)$(OUT) $(FLAGS)
-echo "Building $NAME..."
+echo -e "${green}=>${reset} Building $NAME..."
 $CC $IN -o $DESTDIR$OUT $FLAGS
