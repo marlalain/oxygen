4a9b3eb8d739bbfa8344f196c7dc0d255996aa61
Paulo <elie9alb@gmail.com>
2021-05-13 19:43:13 -0300

    Added error handling

--- src/bin/cat.rs
+++ src/bin/cat.rs
@@ -1,5 +1,5 @@
-use std::{env, fs};
 use seahorse::{App, Context, Flag, FlagType};
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,24 +8,25 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
         .usage("cat file1 [files2+]")
-        .action(cat)
-        .flag(Flag::new("verbose", FlagType::Bool)
-              .description("Becomes more verbose")
-              .alias("v"),
-        )
-        ;
+        .action(|c| match cat(c) {
+            Ok(()) => std::process::exit(1),
+            Err(e) => println!("Could not read file: {}", e),
+        })
+        .flag(
+            Flag::new("verbose", FlagType::Bool)
+                .description("Becomes more verbose")
+                .alias("v"),
+        );
     app.run(args);
 }
 
-fn cat(c: &Context) {
+fn cat(c: &Context) -> std::io::Result<()> {
     for arg in &c.args {
-        let content = fs::read_to_string(arg);
+        let content = fs::read_to_string(arg)?;
         if c.bool_flag("verbose") {
             println!("Opening file {:?}", &arg);
         }
-        match content {
-            Ok(ok) => { print!("{}", ok); }
-            Err(..) => { panic!(); }
-        }
+        print!("{}", content);
     }
+    Ok(())
 }
--- src/bin/touch.rs
+++ src/bin/touch.rs
@@ -1,6 +1,6 @@
-use std::fs::File;
-use std::env;
 use seahorse::{App, Context, Flag, FlagType};
+use std::env;
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,19 +9,24 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
         .usage("touch file1 [files2+]")
-        .action(touch)
-        .flag(Flag::new("verbose", FlagType::Bool)
-              .description("Becomes more verbose")
-              .alias("v"),
+        .action(|c| match touch(c) {
+            Ok(()) => std::process::exit(1),
+            Err(e) => println!("Could not create file: {}", e),
+        })
+        .flag(
+            Flag::new("verbose", FlagType::Bool)
+                .description("Becomes more verbose")
+                .alias("v"),
         );
     app.run(args);
 }
 
-fn touch(c: &Context) {
+fn touch(c: &Context) -> std::io::Result<()> {
     for arg in &c.args {
-        let mut _file = File::create(arg);
+        let mut _file = File::create(arg)?;
         if c.bool_flag("verbose") {
             println!("Created file '{}'.", arg);
         }
     }
+    Ok(())
 }
